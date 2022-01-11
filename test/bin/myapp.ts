b0ae011be23dcbb26e548691d219d65c17e6acb3
Simon-Pierre Gingras <892367+spg@users.noreply.github.com>
2022-01-11 09:22:06 -0500

    Rename stack

--- test/bin/myapp.ts
+++ test/bin/myapp.ts
@@ -4,7 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { MyappStack } from '../lib/myapp-stack';
 
 const app = new cdk.App();
-new MyappStack(app, 'MyappStack', {
+new MyappStack(app, 'PGTest', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
