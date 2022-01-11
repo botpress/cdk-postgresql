754cd313e4300d1762d6966eae92cb72bf58939a
Simon-Pierre Gingras <892367+spg@users.noreply.github.com>
2022-01-11 09:19:38 -0500

    Added CDK test stack

---
+++ test/test/myapp.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Myapp from '../lib/myapp-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/myapp-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Myapp.MyappStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
