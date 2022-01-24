354e5aaa880a7b59ce463c166aba4b189faedcf7
Simon-Pierre Gingras <892367+spg@users.noreply.github.com>
2022-01-24 08:35:01 -0500

    Can start container

--- cdk-postgresql/test/database.test.ts
+++ cdk-postgresql/test/database.test.ts
@@ -1,8 +1,37 @@
+import { CloudFormationCustomResourceEvent } from "aws-lambda";
 import { handler } from "../lib/database.handler";
+import { GenericContainer, StartedTestContainer } from "testcontainers";
+import ms from "ms";
 
-test("something", async () => {
-  console.log({ handler });
-  const event = {};
-  // @ts-ignore
-  await handler(event);
+describe("database", () => {
+  let container: StartedTestContainer;
+
+  beforeAll(async () => {
+    container = await new GenericContainer("postgres")
+      .withExposedPorts(5432)
+      .withEnv("POSTGRES_PASSWORD", "postgres")
+      .start();
+  }, ms("2m"));
+
+  afterAll(async () => {
+    await container.stop();
+  });
+
+  test("something", async () => {
+    console.log({ handler });
+    const event: CloudFormationCustomResourceEvent = {
+      RequestType: "Create",
+      ServiceToken: "",
+      ResponseURL: "",
+      StackId: "",
+      RequestId: "",
+      LogicalResourceId: "",
+      ResourceType: "",
+      ResourceProperties: {
+        ServiceToken: "",
+        Connection: { Host: container.getHost() },
+      },
+    };
+    await handler(event);
+  });
 });
