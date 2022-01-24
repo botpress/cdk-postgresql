db8ce0db9973a578dd344f0fe21934da87b6b859
Simon-Pierre Gingras <892367+spg@users.noreply.github.com>
2022-01-24 08:13:40 -0500

    only run TS tests

--- cdk-postgresql/jest.config.js
+++ cdk-postgresql/jest.config.js
@@ -1,5 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/test"],
+  testMatch: ["**/*.test.ts"],
 };
